a15f17aa75a3103afe0eaf421588314a251b4a7b
Mahmoud <mah2002moud@gmail.com>
2024-06-29 05:33:11 +0300

    Added files:    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 Define a Puppet manifest to install Flask version 2.1.0 using pip3
-package{ 'flesk':
+package{ 'flask':
   ensure   => '2.1.0'
   provider => 'pip3',
-  require  => Package['python3-pip'],
 }
