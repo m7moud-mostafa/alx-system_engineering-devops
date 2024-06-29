a2931230bbabc80f12c49dd6ef1e39bab98c2dc7
Mahmoud <mah2002moud@gmail.com>
2024-06-29 05:17:32 +0300

    Added files:    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-# creates a file in tmp
+# Define a Puppet manifest to create a file in /tmp/school
 file { '/tmp/school':
 	ensure => file,
-	mode => '0744'
-	content => 'I love Puppet'
-	owner => 'www-data'
-	group => 'www-data'
+	mode => '0744',
+	content => 'I love Puppet',
+	owner => 'www-data',
+	group => 'www-data',
 }
